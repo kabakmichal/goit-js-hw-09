c01db21da6f8a45382bb32b19bfd89d7ed4a326c
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-08-02 12:57:28 +0200

    task01 finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const body = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+let changeColor = null;
+
+start.addEventListener('click', () => {
+  start.disabled = true;
+  stop.disabled = false;
+
+  changeColor = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.background = color;
+  }, 1000);
+});
+
+stop.addEventListener('click', () => {
+  stop.disabled = true;
+  start.disabled = false;
+  clearInterval(changeColor);
+});
 
