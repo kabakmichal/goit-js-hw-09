3ce554ca4e04d8cc3d0c38eb44bfdc4a2277c227
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-08-03 16:16:05 +0000

    Deploying to gh-pages from @ kabakmichal/goit-js-hw-09@afa956121bf9670d899ebf4881649e88aea57314 🚀

---
+++ 03-promises.ca070cce.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("button"),o=document.querySelector("delay");e.addEventListener("click",(function(){console.log(o.toNumber())}))}();
+//# sourceMappingURL=03-promises.ca070cce.js.map
