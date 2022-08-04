dfdd3d3d6953080d768d2afbbbbcbb1a176a3c21
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-08-04 14:58:08 +0000

    Deploying to gh-pages from @ kabakmichal/goit-js-hw-09@580a1a8572e7bc07172f186b11e228970ddee479 🚀

--- 03-promises.b1a07b3a.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("button"),o=document.querySelector("delay");e.addEventListener("click",(()=>{console.log(o.toNumber())}));
-//# sourceMappingURL=03-promises.b1a07b3a.js.map
--- 03-promises.ca070cce.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("button"),o=document.querySelector("delay");e.addEventListener("click",(function(){console.log(o.toNumber())}))}();
-//# sourceMappingURL=03-promises.ca070cce.js.map
